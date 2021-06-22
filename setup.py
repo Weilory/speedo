ea41844a0c81fed2fb54bbe204b48e39ce45828d
messiz <54391313+messizqin@users.noreply.github.com>
2021-06-22 14:31:43 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 setup(
 	name='speedo',
-	version='1.0.4',
+	version='1.0.5',
 	description='analyse and illustrate data and equation',
 	long_description=readme(),
 	long_description_content_type='text/markdown',
